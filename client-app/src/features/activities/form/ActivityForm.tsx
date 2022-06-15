ddc236e0c555003e9338b677b2bd71d9f13742cf
Paweł Florczak <pawelfsanok@gmail.com>
2022-06-15 05:32:33 +0200

    CRUD activities from frontend connect with backend and database

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity | undefined;
     closeForm:() => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props) {
 
     const initialState = selectedActivity || {
         id: '',
@@ -37,10 +38,10 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Tytuł' value={activity.title} name='title' onChange={handleInputChange}/>
                 <Form.TextArea placeholder='Opis' value={activity.description} name='description' onChange={handleInputChange}/>
                 <Form.Input placeholder='Kategoria' value={activity.category} name='category' onChange={handleInputChange}/>
-                <Form.Input placeholder='Data' value={activity.date} name='date' onChange={handleInputChange}/>
+                <Form.Input type='date' placeholder='Data' value={activity.date} name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='Miasto' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Miejsce wydarzenia' value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button floatet='right' positive type='submit' content='Zatwierdź'/>
+                <Button loading={submitting}floatet='right' positive type='submit' content='Zatwierdź'/>
                 <Button onClick={closeForm} floatet='right' type='button' content='Anuluj'/>
             </Form>
         </Segment>
